518b8cbc99dcca4ef5db76e098793fe0c2c4648e
benjamin <benjamin.cornejo.meneque@gmail.com>
2023-02-18 15:21:56 +0800

    add new models

--- employee/models.py
+++ employee/models.py
@@ -14,6 +14,17 @@ class Employee(models.Model):
     dateBirth = models.DateField()
     contactNumber = PhoneField()
     email = models.EmailField(max_length=200)
+    Religion = models.CharField(max_length=200,default="christian")
+    Barangay = models.CharField(max_length=200, default="fairview")
+    City = models.CharField(max_length=200, default="quezon city")
+    HouseNumber= models.IntegerField(default=0)
+    Country = models.CharField(max_length=200, default="philippines")
+    Zip = models.IntegerField(default=0)
+    TIN = models.IntegerField(default=0)
+    EmployeeStatus = models.CharField(max_length=200, default="active")
+    Department = models.CharField(max_length=200, default="IT")
+    Job = models.CharField(max_length=200, default="IT")
+    # Manager
 
     def __str__(self):
         return self.firstName
